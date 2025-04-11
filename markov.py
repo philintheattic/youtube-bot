86dc62f8dddf7da01e5fc9a62e43edfe0c8a43dd
Philipp Wachowitz <philipp.wachowitz@gmx.de>
2025-04-11 14:46:32 +0200

    fixed iterating over str

--- markov.py
+++ markov.py
@@ -2,7 +2,8 @@ txt = "This is a sample text based on a tutorial."
 order = 3
 ngrams = []
 
-for i in range(len(txt)):
+for i in range(len(txt)-order+1):
     ngrams.append(txt[i:i+order])
 
-print(ngrams)
+print(len(ngrams))
+print(len(set(ngrams)))
